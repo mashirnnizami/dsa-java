74a70e43e00896ec55b6a3c4c9024a7e5afcd38f
mashirdav123 <mashirdav123@gmail.com>
2024-06-11 07:49:31 +0000

    commit

---
+++ variable.java
@@ -0,0 +1,17 @@
+class variable {
+
+    public static void main(String[] args){
+        int rollno;
+        String name;
+        float marks;
+
+        rollno = 101;
+        name = "John";
+        marks = 98.6f;
+
+        System.out.println("Rollno: " + rollno);
+        System.out.println("Name: " + name);
+        System.out.println("Marks: " + marks);
+    }
+    
+}
